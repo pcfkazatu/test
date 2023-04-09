365ffbebac8770c86aa762810834ddfbc6e58953
pcfkazatu <100610907+pcfkazatu@users.noreply.github.com>
2023-04-09 20:53:40 +0600

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 test = 1
 with open('/home/test/Desktop/CICD/python_script_log.txt', 'a') as f:
-    f.write(f'testing new - {datetime.now().isoformat()} \n')
+    f.write(f'testing new script - {datetime.now().isoformat()} \n')
 
 while True:
 	a=1
